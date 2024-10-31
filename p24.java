ece0477c78bf69f4aa533e5eb3b06e195ce02071
KateGaga <kate10111204@gmail.com>
2024-10-31 12:08:11 +0800

    hw1031

---
+++ p24.java
@@ -0,0 +1,12 @@
+package hw1031;
+
+public class p24 {
+	
+	public static void main(String[]args){
+		for(int i = 1; i <= 5; i++) {
+			System.out.println("第"+i+"次的結果");
+		}
+		System.out.println("迴圈結束");
+	}
+	
+}
